e8bc63875004fe1ee0fe27db6d7462d468fd9343
aryabhupathi <108666879+aryabhupathi@users.noreply.github.com>
2022-07-04 16:34:43 +0530

    Add files via upload

---
+++ calci.js
@@ -0,0 +1,19 @@
+function display(val) {
+    document.getElementById('result').value += val;
+    return val;
+}
+function solve() {
+    let x = document.getElementById('result').value;
+    let y = eval(x);
+    document.getElementById('result').value = y;
+    return y;
+}
+function del(){
+    let x = document.getElementById('result').value;
+    let y = x.slice(0, -1);
+    document.getElementById('result').value = y;
+    return y;
+}
+function clearScreen() {
+    document.getElementById('result').value = "";
+}
